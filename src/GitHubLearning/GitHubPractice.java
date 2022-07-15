6787e69d34bb9c4dd9c2da61209b941a345132bc
pramo <pramo@DESKTOP-352M94G.attlocal.net>
2022-07-14 23:50:19 -0700

    FirstCommit

---
+++ src/GitHubLearning/GitHubPractice.java
@@ -0,0 +1,10 @@
+package GitHubLearning;
+
+public class GitHubPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("GitHubPractice started...!!");
+	}
+
+}
